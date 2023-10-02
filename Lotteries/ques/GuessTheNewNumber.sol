bcbf402d84afd60e19f9f97f8b32f4af66734bcf
Aryan Malik <aryanhunny98@gmail.com>
2023-10-02 22:04:13 +0530

    Beginning GuessTheNewNumber

---
+++ Lotteries/ques/GuessTheNewNumber.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract GuessTheNewNumberChallenge {
+    bool public isPassed;
+    function guess(uint8 n) public payable {
+        require(msg.value == 1 ether);
+        uint8 answer = uint8(uint256(keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp))));
+
+        if (n == answer) {
+            isPassed = true;
+        }
+    }
+}
