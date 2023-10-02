7774e1b5f81b9ccb6a73917b1060e3bbe0bfc8e0
Aryan Malik <aryanhunny98@gmail.com>
2023-10-02 22:25:50 +0530

    Completed GuessTheNewNumber

--- Lotteries/ques/GuessTheNewNumber.sol
+++ Lotteries/ques/GuessTheNewNumber.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.0;
 
 contract GuessTheNewNumberChallenge {
     bool public isPassed;
-    function guess(uint8 n) public payable {
-        require(msg.value == 1 ether);
+    function guess(uint8 n) public {
         uint8 answer = uint8(uint256(keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp))));
 
         if (n == answer) {
