49c0506a25aafe392a267453310c91ced2dbe0d6
Aryan Malik <aryanhunny98@gmail.com>
2023-10-10 19:08:32 +0530

    Completed Public Key

--- Accounts/ques/PublicKey.sol
+++ Accounts/ques/PublicKey.sol
@@ -1,3 +1,9 @@
+/*
+    Recall that an address is the last 20 bytes of the keccak-256 hash of the address's public key.
+
+    To complete this challenge, find the public key for the owner's account.
+*/
+
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
